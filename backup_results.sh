b5ec5aa3f6c391507cb07d1c87f8448f6f768582
Ulku Huma Altindag <51422853+uha0001@users.noreply.github.com>
2026-07-20 20:48:46 +0200

    Document backup process in backup_results.sh
    Added comments to explain the backup process.

--- backup_results.sh
+++ backup_results.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Create a compressed archive of the "results" directory.
+# The backup file will be named "results_backup.tar.gz".
+
+# tar  : Create an archive.
+# -c   : Create a new archive.
+# -z   : Compress the archive using gzip.
+# -f   : Specify the output filename.
+# results_backup.tar.gz : Name of the backup archive.
+# results/ : Folder to include in the backup.
+
 tar -czf results_backup.tar.gz results/
