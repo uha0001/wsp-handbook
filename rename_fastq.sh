1c5583980cd2457058ecc15a84d4f2914ca1a82c
Ulku Huma Altindag <51422853+uha0001@users.noreply.github.com>
2026-07-20 21:04:28 +0200

    Add script to rename FASTQ files with underscores
    This script renames all compressed FASTQ files by replacing spaces in their filenames with underscores.

--- rename_fastq.sh
+++ rename_fastq.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Rename all compressed FASTQ files by replacing
+# spaces in their filenames with underscores.
+
+# Loop through every file ending in .fastq.gz
 for f in *.fastq.gz
 do
+    # Rename the file.
+    #
+    # "$f"        = the current filename
+    # ${f// /_}   = replace every space with an underscore
+
     mv "$f" "${f// /_}"
 done
+
